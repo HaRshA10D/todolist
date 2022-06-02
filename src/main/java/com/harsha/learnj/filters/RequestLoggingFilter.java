54c0def3fecfcd3dabd574c53516cfc99cca458f
harsha10d <harshavardhanhd33@gmail.com>
2022-06-02 22:25:44 +0530

    add a webfilter to log the request

---
+++ src/main/java/com/harsha/learnj/filters/RequestLoggingFilter.java
@@ -0,0 +1,35 @@
+package com.harsha.learnj.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
+@WebFilter("/*")
+public class RequestLoggingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        log.info("new request: " + request.getMethod() + " " + request.getRequestURI());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
